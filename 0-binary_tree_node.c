e2789cb73a517d6011791ae50afaf3231356f724
maitha1-nule <nuleahmed@gmail.com>
2024-02-27 23:14:37 +0300

    In this task I have created a function that creates a new binary tree node from the protoype given. The fuction returns a pointer to a new node or null on failure

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t new_node;
 
-	new_node = malloc(sizeof(new_node));
+	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 		return (NULL);
--- binary_trees.h
+++ binary_trees.h
@@ -13,7 +13,7 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
@@ -21,9 +21,8 @@ typedef struct binary_tree_s
     struct binary_tree_s *right;
 };
 
-binary_tree_t;
+typedef struct binary_tree_s binary_tree_t;
 
-void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
