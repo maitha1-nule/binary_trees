f05dcf8c70bb14f4d2ce11161ab26f960e8cc669
maitha1-nule <nuleahmed@gmail.com>
2024-02-28 00:22:39 +0300

    IN this task i have inserted a node as the left child of another node. and in a scenario the left node is not empty then the new node takes the value of the existing left node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,8 +13,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
-
 	if (parent == NULL)
 		return (NULL);
 
