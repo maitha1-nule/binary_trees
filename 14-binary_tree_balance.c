348f46fef72bb3c6dfa7e634fc5f586fcbcbbd02
maitha1-nule <nuleahmed@gmail.com>
2024-03-01 01:01:17 +0300

    in this task i've come up with a function which measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,39 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
- *
- * @tree: A pointer to the root node of the tree to measure the height.
- *
- * Return: If tree is NULL, return 0, else return height.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-
-	size_t left_height = 0, right_height = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height) ? left_height + 1 : right_height + 1);
-}
-
-/**
  * binary_tree_balance - Measures the balance factor of a binary tree.
  *
  * @tree: A pointer to the root node of the tree to measure the balance factor.
  *
- * Return: If tree is NULL, return 0, else return the balance factor.
+ * Return: If tree is NULL, return 0, else return balance factor.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-	return (left_height - right_height);
+	return (0);
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0, else return height.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t left_height = 0, right_height = 0;
+
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+	return (0);
 }
