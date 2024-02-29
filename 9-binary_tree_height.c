9f14e445f425f71f69c77dd3bf0b5a8c82f82275
maitha1-nule <nuleahmed@gmail.com>
2024-02-29 22:37:25 +0300

    In this task ive created a fuction that counts the nodes with atleast 1 child in a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,16 +5,17 @@
  *
  *@tree: this is the pointer to the tree to get the height
  *
- *Return: 0 if tree is NULL
+ *Return: 0 if tree is NULL else height
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t right_height, left_height;
+	size_t right_height = 0, left_height = 0;
 
 	if (tree == NULL)
 		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	return (left_height > right_height ? left_height : right_height) + 1;
+	if (left_height > right_height)
+		return (left_height + 1);
+	return (right_height + 1);
 }
-
