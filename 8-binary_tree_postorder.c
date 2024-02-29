b23c0d6f50fc18f1b8b6f538c69f1ce1f95ab0eb
maitha1-nule <nuleahmed@gmail.com>
2024-02-29 12:47:06 +0300

    in this task i have come up with a scriot that tranverses a tree using post order technique

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include"binary_trees.h"
+
+/**
+ * binary_tree_postorder: tranverses the tree using postorder
+ *
+ * @tree: this is a pointer to the tree to tranverse
+ * @func: this is a function pointer to a function for each node
+ *
+ * Return: nothing if tree or func is NULL
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
